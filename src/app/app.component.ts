4d29785c51d74d964055e4413d7f8568b5102e45
SergiGan <42737988+SergiGan@users.noreply.github.com>
2022-10-14 13:32:12 +0300

    Update app.component.ts
    add new product links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,40 +22,40 @@ export class AppComponent {
 
 products = [{
     id:1,
-    image: 'http://lorempixel.com/400/200/technics',
+    image: 'https://www.pexels.com/uk-ua/search/technic/',
     name: 'technics' ,
     describe: 'This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.',
     price: '400$'
   }, {
     id:2,
-    image: 'http://lorempixel.com/400/200/food',
+    image: 'https://unsplash.com/t/food-drink/',
     name: 'food',
     describe: 'This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.',
     price: '450$'
   }, {
     id:3,
-    image: 'http://lorempixel.com/400/200/transport',
+    image: 'https://www.pexels.com/uk-ua/collections/cars-dvgfira/',
     name: 'transport',
     describe: 'This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.',
     price: '500$'
   }, 
     {
       id:4,
-    image: 'http://lorempixel.com/400/200/fashion',
+    image: 'https://www.pexels.com/uk-ua/search/fassion/',
     name: 'fashion',
     describe: 'This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.',
     price: '550$'
   },
     {
       id:5,
-    image: 'http://lorempixel.com/400/200/animals',
+    image: 'https://www.pexels.com/uk-ua/search/animals/',
     name: 'animals',
     describe: 'This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.',
     price: '600$'
   },
   {
       id:6,
-    image: 'http://lorempixel.com/400/200/sports',
+    image: 'https://www.pexels.com/uk-ua/search/sports/',
     name: 'sports',
     describe: 'This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.',
     price: '700$',
